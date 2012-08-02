c98617517a20c8209a70bca20b67271e7687b35c
vidhya <vidhya_d@pec.edu>
2012-08-02 16:02:18 +0530

    Strikethrough after discount

--- src/com/thoughtworks/hp/adapters/BuyListAdapter.java
+++ src/com/thoughtworks/hp/adapters/BuyListAdapter.java
@@ -1,18 +1,18 @@
 package com.thoughtworks.hp.adapters;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import android.content.Context;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import com.thoughtworks.hp.R;
 import com.thoughtworks.hp.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class BuyListAdapter extends ArrayAdapter<Product> {
 
@@ -21,12 +21,15 @@ public class BuyListAdapter extends ArrayAdapter<Product> {
     private Context context;
     private int layout;
     private static final String INDIAN_CURRENCY_SYMBOL = "Rs. ";
+    private static final String MULTIPLE_ITEMS = " nos";
+    private static final String SINGLE_ITEM = " no";
 
     private static class ViewHolder {
         TextView productName;
         TextView productUOM;
-        TextView productPrice;
+        TextView productCost;
         TextView productQuantity;
+        TextView amountAfterDiscount;
     }
 
     public BuyListAdapter(Context context, int layout, Map<Product,Integer> productQuantityMap, List<Product> products) {
@@ -54,18 +57,28 @@ public class BuyListAdapter extends ArrayAdapter<Product> {
     }
 
     private void populateProductDetailsForLineItem(ViewHolder holder, Product product, int quantity) {
+
         TextView productNameTextView = holder.productName;
         productNameTextView.setText(product.getName());
 
-        TextView productCostTextView = holder.productPrice;
-        String costAsString = INDIAN_CURRENCY_SYMBOL + Double.toString(Math.round(product.getPrice() * quantity));
+        TextView productCostTextView = holder.productCost;
+        double productCost = Math.round(product.getPrice() * quantity);
+        String costAsString = INDIAN_CURRENCY_SYMBOL + Double.toString(productCost);
         productCostTextView.setText(costAsString);
+
 
         TextView productUOMTextView = holder.productUOM;
         productUOMTextView.setText(product.getUom());
 
         TextView textView=holder.productQuantity;
-        textView.setText(String.valueOf(quantity));
+        String productQuantityUnit = quantity==1 ? SINGLE_ITEM: MULTIPLE_ITEMS ;
+        textView.setText(String.valueOf(quantity)+ productQuantityUnit);
+        if(product.getId() == 2) {
+            TextView amountAfterDiscount = holder.amountAfterDiscount;
+            amountAfterDiscount.setText(INDIAN_CURRENCY_SYMBOL + String.valueOf(Math.round(productCost * 0.8)));
+            productCostTextView.setPaintFlags(productCostTextView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        }
     }
 
     private View setTagWithViewHolder(View convertView) {
@@ -74,10 +87,10 @@ public class BuyListAdapter extends ArrayAdapter<Product> {
         convertView = view.inflate(layout, null);
 
         holder.productName = (TextView) convertView.findViewById(R.id.product_name);
-        holder.productPrice = (TextView) convertView.findViewById(R.id.product_price);
+        holder.productCost = (TextView) convertView.findViewById(R.id.product_price);
         holder.productUOM = (TextView) convertView.findViewById(R.id.product_uom);
         holder.productQuantity = (TextView) convertView.findViewById(R.id.product_quantity);
-
+        holder.amountAfterDiscount = (TextView) convertView.findViewById(R.id.discount_price);
         convertView.setTag(holder);
         return convertView;
     }
